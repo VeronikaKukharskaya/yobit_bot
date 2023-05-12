db0319f1d32d1881cc964308c393381b766f0142
Veronika Kukharskaya <verka04052001@gmail.com>
2023-05-12 23:29:21 +0300

    созданы метод об актуальных парах на бирже(get_info) и метод о парах за последние 24 часа(get_ticker)

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def get_info():
+    response = requests.get(url="https://yobit.net/api/3/info")
+
+    with open("info.txt", "w") as file:
+        file.write(response.text)
+
+    return response.text
+
+
+def get_ticker():
+    response = requests.get(url="https://yobit.net/api/3/ticker/eth_btc-xrp_btc")
+
+    with open("ticker.txt", "w") as file:
+        file.write(response.text)
+
+    return response.text
+
+
+def main():
+    # print(get_info())
+    print(get_ticker())
+
+
+if __name__ == '__main__':
+    main()
