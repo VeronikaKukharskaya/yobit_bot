853e05816025f4bc755c2bab17c609e72c5cd90a
Veronika Kukharskaya <verka04052001@gmail.com>
2023-05-13 00:22:31 +0300

    get_trades - получаем уже совершённые сделки по покупке и продаже

--- main.py
+++ main.py
@@ -37,13 +37,23 @@ def get_depth(coin1="btc", coin2="usd", limit=150):
     return f"Total bids: {total_bids_amount} $"
 
 
+def get_trades(coin1="btc", coin2="usd", limit=150):
+    response = requests.get(url=f"https://yobit.net/api/3/trades/{coin1}_{coin2}?limit={limit}&ignore_invalid=1")
+
+    with open("trades.txt", "w") as file:
+        file.write(response.text)
+
+    return response.text
 
 
 def main():
     # print(get_info())
     # print(get_ticker())
     # print(get_ticker(coin1="eth"))
-    print(get_depth())
+    # print(get_depth())
+    # print(get_depth(coin1="doge"))
+    # print(get_depth(coin1="doge", limit=2000))
+    print(get_trades())
 
 
 if __name__ == '__main__':
