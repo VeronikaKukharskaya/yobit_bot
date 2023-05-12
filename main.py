609927057937930da411ee0c934dbe16de582949
Veronika Kukharskaya <verka04052001@gmail.com>
2023-05-13 00:26:25 +0300

    получаем общую сумму купленных и проданных монет

--- main.py
+++ main.py
@@ -43,7 +43,18 @@ def get_trades(coin1="btc", coin2="usd", limit=150):
     with open("trades.txt", "w") as file:
         file.write(response.text)
 
-    return response.text
+    total_trade_ask = 0
+    total_trade_bid = 0
+
+    for item in response.json()[f"{coin1}_{coin2}"]:
+        if item["type"] == "ask":
+            total_trade_ask += item["price"] * item["amount"]
+        else:
+            total_trade_bid += item["price"] * item["amount"]
+
+    info = f"[-] TOTAL {coin1} SELL: {round(total_trade_ask, 2)} $\n[+] TOTAL {coin1} BUY: {round(total_trade_bid, 2)} $"
+
+    return info
 
 
 def main():
